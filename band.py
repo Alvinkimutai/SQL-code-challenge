bd881f9c5893df4c3fcbe01ec8b7be46e93d222b
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-09-17 08:22:44 +0300

    finishes on one method and two class methods

--- band.py
+++ band.py
@@ -19,7 +19,7 @@ class Band:
     def create_table(cls):
        connection = sqlite3.connect("mydb.db")
        cursor = connection.cursor()
-       cursor.execute('''CREATE TABLE IF NOT EXIST band 
+       cursor.execute('''CREATE TABLE IF NOT EXIST bands
                       (id INTEGER PRIMARY KEY AUTOINCREAMENT,
                       name TEXT, 
                       hometown TEXT)''')
@@ -29,7 +29,7 @@ class Band:
     def create(cls, name, hometown):
        connection = sqlite3.connect("mydb.db")
        cursor = connection.cursor()
-       cursor.execute("insert into band (name, hometown) values(?,?)", (name, hometown))
+       cursor.execute("insert into bands (name, hometown) values(?,?)", (name, hometown))
        connection.commit()
        connection.close
     
@@ -48,6 +48,39 @@ class Band:
                       WHERE concerts.band_id = ?''', (self.id,)).fetchall()
        connection.commit()
        connection.close
+    def play_in_venue(self, venue, date):
+       connection = sqlite3.connect("mydb.db")
+       cursor = connection.cursor()
+       cursor.execute('INSERT INTO concerts(band_id, venue_id, date) VALUES(?,?,?)', (self.id, venue.id, date))
+       connection.commit()
+       connection.close
+    @classmethod
+    def all_introductions(cls):
+       connection = sqlite3.connect("mydb.db")
+       cursor = connection.cursor()
+       cursor.execute('''SELECT concert.date, venue.city, bands.name, bands.hometown
+                      FROM concerts
+                      JOIN venues on concerts.venue id = venues id
+                      JOIN bands on concert.band id = bands id'''
+                      )
+       intro = cursor.fetchall()
+       connection.close
+       return [f"Hello {city}!!!!! We are {name} and we're from {hometown}" for city, name, hometown in intro]
+    
+    @classmethod
+    def most_performances(cls):
+       connection = sqlite3.connect("mydb.db")
+       cursor = connection.cursor()
+       cursor.execute('''SELECT band id, COUNT(*) as performance_count
+                      FROM concerts
+                      GROUP BY band_id
+                      ORDER BY performance_count DESC
+                      LIMIT 1''')
+       result = cursor.fetchone()
+       connection.close()
+       return result[0] if result else None
+       
+       
     
     
     
